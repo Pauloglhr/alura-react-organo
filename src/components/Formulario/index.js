11c2c576a89a8e09f16bfc73174def7244d71102
Pauloglhr <pauloglhr@outlook.com>
2024-11-13 17:18:36 -0300

    Desenvolvido components de Botao e ListaSuspensa, vinculação dos mesmos ao formulário e adicionado evento para ouvir submissão do formulário.

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,14 +1,36 @@
 import './Formulario.css'
 import Inputs from '../Inputs'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('O formulário foi submetido.');
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador:</h2>
-                <Inputs label="Nome" placeholder="Digite o seu nome"/>
-                <Inputs label="Cargo" placeholder="Digite o seu cargo"/>
+                <Inputs obrigatorio={true} label="Nome" placeholder="Digite o seu nome"/>
+                <Inputs obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo"/>
                 <Inputs label="Imagem" placeholder="Informe o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
